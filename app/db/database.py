7699b920d21fc2ac28db44081570decb8c7f8b26
qnderkk <Qndi@yandex.ru>
2026-05-14 23:13:23 +0300

    add db and config

---
+++ app/db/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+from app.core.config import settings
+
+
+engine = create_async_engine(
+    bind=settings.database_url,
+    echo=True
+)
+
+session_factory = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+class Base(DeclarativeBase):
+    pass
+
